904a708e6471795180be8cc787e00c08d60fe94a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-05 12:04:33 -0600

    udpate

--- index.php
+++ index.php
@@ -274,7 +274,7 @@ if(isset($_POST['exp'])){
                 </div>
                 <div class="mb-3">
                     <label for="amount" class="form-label text-white">Amount &nbsp;<span style="font-size: 10px; color: rgb(169, 169, 169);">e.g 130.40</span></label>
-                    <input type="number" name="amount" class="form-control" id="amount">
+                    <input type="text" name="amount" class="form-control" id="amount">
                 </div>
                 <div class="mb-3">
                     <label for="date" class="form-label text-white">Date Spent</label>
