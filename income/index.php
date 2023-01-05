9b4db2ea62992027ecae61bc7b81ba27e24c5a6d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-05 13:19:14 -0600

    udpate

--- income/index.php
+++ income/index.php
@@ -50,7 +50,7 @@ session_start();
             <hr>
             <div class="mt-4"></div>
 
-            <table class="table table-bordered">
+            <table class="datatable table table-bordered">
                 <thead style="background-color: white;">
                   <tr>
                     <th class="text-center" scope="col-1">ID #</th>
@@ -98,6 +98,28 @@ session_start();
             </table>
 
     </div> 
+
+    <script type="text/javascript">
+		$(document).ready(function() {
+			$('.datatable').dataTable({
+				"sPaginationType": "bs_four_button"
+			});	
+			$('.datatable').each(function(){
+				var datatable = $(this);
+				// SEARCH - Add the placeholder for Search and Turn this into in-line form control
+				var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
+				search_input.attr('placeholder', 'Search');
+				search_input.addClass('form-control input-sm');
+				// LENGTH - Inline-Form control
+				var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
+				length_sel.addClass('form-control input-sm');
+			});
+		});
+		</script>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js"></script>
 
     <!-- custom script -->
     <script src="script.js"></script>
