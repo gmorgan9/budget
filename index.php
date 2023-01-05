9402bbf8c810ab286b9332d884689464c4fe837e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-05 16:29:50 -0600

    update

--- index.php
+++ index.php
@@ -219,8 +219,8 @@ session_start();
                     </h3>
                     <div class="card" style="margin-right: 20px; width: 40%; background-color: rgb(100, 100, 100) !important; color: white;">
                         <div class="card-body">
-                          <h5 class="card-title text-center">Monthly</h5>
-                          <p class="card-text fs-6 text-center">
+                          <h3 class="card-title text-center">Monthly</h3>
+                          <p class="card-text fs-5 text-center">
                             <?php 
                             $month_year = date('F Y');
                             $firstday = strtotime("first day of ". $month_year);
