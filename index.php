10a42c9d35141ae1df1d1b358270331e74ceb93c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-05 11:22:18 -0600

    update

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_assoc($result)) {
             $firstname    = $row['firstname'];
             $loggedin     = $row['loggedin'];
-            $acct_type    = $row['isadmin'];
+            $role    = $row['isadmin'];
     }}
 
     ?>
@@ -298,6 +298,9 @@ if(mysqli_num_rows($result) > 0){
             <div class="mt-5"></div>
             <div class="d-flex justify-content-center">
                 <ul class="list-group list-group-flush w-75">
+                    <?php if($role === 1) { ?>
+                        <a style="text-decoration: none;" href="users/"><li class="list-group-item text-white text-center" style="background-color: rgb(100, 100, 100) !important;">Users</li></a>
+                    <?php } else {}?>
                     <a style="text-decoration: none;" href="expenses/"><li class="list-group-item text-white text-center" style="background-color: rgb(100, 100, 100) !important;">Expenses</li></a>
                     <a style="text-decoration: none;" href="income/"><li class="list-group-item text-white text-center" style="background-color: rgb(100, 100, 100) !important;">Income</li></a>
                     <a style="text-decoration: none;" href="account/"><li class="list-group-item text-white text-center" style="background-color: rgb(100, 100, 100) !important;">Edit Account</li></a>
