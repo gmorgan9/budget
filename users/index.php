796496236e155ba9a945e5d2306a04e37fc5ef91
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 10:43:01 -0600

    update

--- users/index.php
+++ users/index.php
@@ -51,7 +51,7 @@ date_default_timezone_set('America/Denver');
             </p>
             <hr>
             <div class="mt-4"></div>
-            
+
             <table class=" table table-bordered">
                 <thead style="background-color: white;">
                   <tr>
@@ -107,9 +107,10 @@ date_default_timezone_set('America/Denver');
     </div> 
 
 
-    <!-- custom script -->
+<!-- custom script -->
     <script src="script.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<!-- end custom script -->
 </body>
 </html>
