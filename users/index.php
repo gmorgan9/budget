0a6011d1d365461ad49948735c1c2b0fb102d145
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 16:47:44 -0600

    update

--- users/index.php
+++ users/index.php
@@ -34,11 +34,11 @@ date_default_timezone_set('America/Denver');
     <!-- end bootstrap -->
 
     <!-- custom styles -->
-        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="../style.css?v=1.25">
     <!-- end custome styles -->
 
 </head>
-<body style="background-color: rgb(78, 78, 78);">
+<body style="background-color: #3e4881;">
 
     <div class="container">
 
