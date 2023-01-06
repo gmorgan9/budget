7e6ccc6c7c3456056b18639495f47fc8f573dcb0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 09:25:32 -0600

    udpate

--- index.php
+++ index.php
@@ -182,7 +182,7 @@ session_start();
                 <input style="background-color: #3e4881 !important; border-radius: 15px !important; width: 70%; margin-left: auto; margin-right: auto;" type="submit" name="login" class="btn text-white fw-bold" value="Log In">
             </div>
         </form>
-        <div class="d-flex justify-content-center" style="background-color: pink;width: 70%;">
+        <div class="d-flex justify-content-end" style="background-color: pink; width: 70%;">
             <p class="text-muted text-end">
                 Forgot Pin?
             </p>
