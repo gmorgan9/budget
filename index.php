c202a5c2e99a4e8681ab185ca32455ee24c727ae
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 16:35:51 -0600

    update

--- index.php
+++ index.php
@@ -718,6 +718,17 @@ session_start();
 <?php } ?>
 
 <!-- custom script -->
+    <script>
+        $(function()
+{
+    window.onorientationchange = OrientationChanged;
+    window.setTimeout(OrientationChanged, 0);
+}
+function OrientationChanged(e)
+{
+    $('body').css('-webkit-transform', window.orientation % 180 == 0 ? '' : 'rotate(-90deg)');
+}
+    </script>
     <script src="script.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
