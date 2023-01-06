1973c941b5cf26306a91c0bfc3b2fbe0b3f502c3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 16:42:41 -0600

    update

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ session_start();
     <!-- end bootstrap -->
 
     <!-- custom styles -->
-        <link media="all and (orientation:portrait)" rel="stylesheet" href="style.css?v=1.25">
+        <link rel="stylesheet" href="style.css?v=1.25">
         <style>
             .success {
                 width: 70%;
@@ -718,6 +718,9 @@ session_start();
 <?php } ?>
 
 <!-- custom script -->
+    <script>
+        screen.lockOrientation('portrait');
+    </script>
     <script src="script.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
