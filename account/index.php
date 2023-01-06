fdc8425ade30d0165ec6276f6a230ed8f7383212
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 10:47:56 -0600

    update

--- account/index.php
+++ account/index.php
@@ -3,86 +3,88 @@ require_once "../database/connection.php";
 require_once "../path.php";
 session_start();
 ?>
-<?php
-// login
-if(isset($_POST['login'])){
-    // $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
-    $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-    $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-    $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pin = $_POST['pin'];
-    $isadmin = $_POST['isadmin'];
-    $loggedin = $_POST['loggedin'];
-    $account_link = $_POST['account_link'];
-    
-    $select = " SELECT * FROM users WHERE account_link = '$account_link' && pin = '$pin' ";
-    
-    $result = mysqli_query($conn, $select);
-    
-    if(mysqli_num_rows($result) > 0){
-    
-       $row = mysqli_fetch_array($result);
-       $sql = "UPDATE users SET loggedin='1' WHERE pin='$pin'";
-       if (mysqli_query($conn, $sql)) {
-          echo "Record updated successfully";
-        } else {
-          echo "Error updating record: " . mysqli_error($conn);
-        }
-        $_SESSION['firstname']        = $row['firstname'];
-        $_SESSION['user_id']          = $row['user_id'];
-        $_SESSION['loggedin']         = $row['loggedin'];
-        $_SESSION['user_idno']        = $row['idno'];
-        $_SESSION['lastname']         = $row['lastname'];
-        $_SESSION['username']         = $row['username'];
-        $_SESSION['email']            = $row['email'];
-        $_SESSION['pin']             = $row['pin'];
-        // $_SESSION['cpass']            = $row['cpassword'];
-        // header('location:' . BASE_URL . '/');
-        header('location: /');
-    
-    }else{
-       $error = '
-       <div class="pt-3"></div>
-       <div class="login_error">
-       <strong>Error:</strong> 
-       The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
-       </div>
-       ';
-    }
+<!-- php functions -->
+    <?php
+    // login
+        if(isset($_POST['login'])){
+            // $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
+            $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+            $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+            $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $pin = $_POST['pin'];
+            $isadmin = $_POST['isadmin'];
+            $loggedin = $_POST['loggedin'];
+            $account_link = $_POST['account_link'];
 
-};
-// end login
+            $select = " SELECT * FROM users WHERE account_link = '$account_link' && pin = '$pin' ";
 
-// update user
-if(isset($_POST['update_acc'])){
-    $idno  = rand(10000, 99999);
-    $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-    $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pin = $_POST['pin'];
+            $result = mysqli_query($conn, $select);
+
+            if(mysqli_num_rows($result) > 0){
+            
+               $row = mysqli_fetch_array($result);
+               $sql = "UPDATE users SET loggedin='1' WHERE pin='$pin'";
+               if (mysqli_query($conn, $sql)) {
+                  echo "Record updated successfully";
+                } else {
+                  echo "Error updating record: " . mysqli_error($conn);
+                }
+                $_SESSION['firstname']        = $row['firstname'];
+                $_SESSION['user_id']          = $row['user_id'];
+                $_SESSION['loggedin']         = $row['loggedin'];
+                $_SESSION['user_idno']        = $row['idno'];
+                $_SESSION['lastname']         = $row['lastname'];
+                $_SESSION['username']         = $row['username'];
+                $_SESSION['email']            = $row['email'];
+                $_SESSION['pin']             = $row['pin'];
+                // $_SESSION['cpass']            = $row['cpassword'];
+                // header('location:' . BASE_URL . '/');
+                header('location: /');
+            
+            }else{
+               $error = '
+               <div class="pt-3"></div>
+               <div class="login_error">
+               <strong>Error:</strong> 
+               The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
+               </div>
+               ';
+            }
+
+        };
+    // end login
+
+    // update account
+        if(isset($_POST['update_acc'])){
+            $idno  = rand(10000, 99999);
+            $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+            $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $pin = $_POST['pin'];
 
 
-    date_default_timezone_set('America/Denver');
-    $date = date('F d, Y, g:i a', time());
+            date_default_timezone_set('America/Denver');
+            $date = date('F d, Y, g:i a', time());
 
-    $insert = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', username = '$username', account_link = '$account_link', email = '$email', pin = '$pin' WHERE user_id = '".$_POST['user_id']."'";
-    mysqli_query($conn, $insert);
-    //header("location: /");
-    $success = '
-       <div class="pt-3"></div>
-       <div class="login_success">
-       <strong>Success:</strong> 
-       Your account has successfully been updated!
-       </div>
-       ';
+            $insert = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', username = '$username', account_link = '$account_link', email = '$email', pin = '$pin' WHERE user_id = '".$_POST['user_id']."'";
+            mysqli_query($conn, $insert);
+            //header("location: /");
+            $success = '
+               <div class="pt-3"></div>
+               <div class="success">
+               <strong>Success:</strong> 
+               Your account has successfully been updated!
+               </div>
+               ';
 
-  };
-// end update income
-?>
+        };
+    // end update account
+    ?>
+<!-- end php functions -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -104,11 +106,8 @@ if(isset($_POST['update_acc'])){
 
     <!-- custom styles -->
         <link rel="stylesheet" href="../style.css">
-    <!-- end custom styles -->
-
-    <style>
-
-            .login_success {
+        <style>
+            .success {
                 width: 95%;
                 border-left: 4px solid #00a32a;
                 padding: 12px;
@@ -120,28 +119,14 @@ if(isset($_POST['update_acc'])){
                 box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
                 word-wrap:break-word
             }
-            /* .login .success {
-                border-left-color:#00a32a
-            } 
-            .login_success {
-                border-left-color:#36d639;
-            } */
-</style>
+        </style>
+    <!-- end custom styles -->
 
 </head>
 <body style="background-color: rgb(78, 78, 78);">
 
-
-    <?php 
-    // $id = $_SESSION['user_id'];
-    // $select2 = " SELECT * FROM users WHERE user_id = '$id' ";
-    // $result2 = mysqli_query($conn, $select2);
-    // if (mysqli_num_rows($result2) > 0) {
-    //     while($row2 = mysqli_fetch_assoc($result2)) {
-    //         $account_link    = $row2['account_link'];
-    // }}
-
-
+<!-- php code -->
+   <?php 
     $user_id = $_GET['id'];
     $select = " SELECT * FROM users WHERE user_id = '$user_id'";
     $result = mysqli_query($conn, $select);
@@ -156,15 +141,12 @@ if(isset($_POST['update_acc'])){
             $account_link   = $row['account_link'];
             $pin       = $row['pin'];
     }}
-
     ?>
 
-    <!-- end php code -->
+<!-- end php code -->
 
 
     <div class="container">
-
-        <!-- <section class="" data-page="income"> -->
             <div class="mt-4"></div>
             <h2 class="text-white">
                 Edit Account
@@ -215,9 +197,10 @@ if(isset($_POST['update_acc'])){
 
     </div>
 
-    <!-- custom script -->
+<!-- custom script -->
     <script src="script.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<!-- end custom script -->
 </body>
 </html>
--- users/view/index.php
+++ users/view/index.php
@@ -4,86 +4,88 @@ require_once "../../path.php";
 session_start();
 date_default_timezone_set('America/Denver');
 ?>
-<?php
-// login
-    if(isset($_POST['login'])){
-        // $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
-        $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-        $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-        $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $pin = $_POST['pin'];
-        $isadmin = $_POST['isadmin'];
-        $loggedin = $_POST['loggedin'];
-        $account_link = $_POST['account_link'];
-        
-        $select = " SELECT * FROM users WHERE account_link = '$account_link' && pin = '$pin' ";
-        
-        $result = mysqli_query($conn, $select);
-        
-        if(mysqli_num_rows($result) > 0){
-        
-           $row = mysqli_fetch_array($result);
-           $sql = "UPDATE users SET loggedin='1' WHERE pin='$pin'";
-           if (mysqli_query($conn, $sql)) {
-              echo "Record updated successfully";
-            } else {
-              echo "Error updating record: " . mysqli_error($conn);
+<!-- php functions -->
+    <?php
+    // login
+        if(isset($_POST['login'])){
+            // $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
+            $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+            $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+            $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $pin = $_POST['pin'];
+            $isadmin = $_POST['isadmin'];
+            $loggedin = $_POST['loggedin'];
+            $account_link = $_POST['account_link'];
+
+            $select = " SELECT * FROM users WHERE account_link = '$account_link' && pin = '$pin' ";
+
+            $result = mysqli_query($conn, $select);
+
+            if(mysqli_num_rows($result) > 0){
+            
+               $row = mysqli_fetch_array($result);
+               $sql = "UPDATE users SET loggedin='1' WHERE pin='$pin'";
+               if (mysqli_query($conn, $sql)) {
+                  echo "Record updated successfully";
+                } else {
+                  echo "Error updating record: " . mysqli_error($conn);
+                }
+                $_SESSION['firstname']        = $row['firstname'];
+                $_SESSION['user_id']          = $row['user_id'];
+                $_SESSION['loggedin']         = $row['loggedin'];
+                $_SESSION['user_idno']        = $row['idno'];
+                $_SESSION['lastname']         = $row['lastname'];
+                $_SESSION['username']         = $row['username'];
+                $_SESSION['email']            = $row['email'];
+                $_SESSION['pin']             = $row['pin'];
+                // $_SESSION['cpass']            = $row['cpassword'];
+                // header('location:' . BASE_URL . '/');
+                header('location: /');
+            
+            }else{
+               $error = '
+               <div class="pt-3"></div>
+               <div class="login_error">
+               <strong>Error:</strong> 
+               The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
+               </div>
+               ';
             }
-            $_SESSION['firstname']        = $row['firstname'];
-            $_SESSION['user_id']          = $row['user_id'];
-            $_SESSION['loggedin']         = $row['loggedin'];
-            $_SESSION['user_idno']        = $row['idno'];
-            $_SESSION['lastname']         = $row['lastname'];
-            $_SESSION['username']         = $row['username'];
-            $_SESSION['email']            = $row['email'];
-            $_SESSION['pin']             = $row['pin'];
-            // $_SESSION['cpass']            = $row['cpassword'];
-            // header('location:' . BASE_URL . '/');
-            header('location: /');
+
+        };
+    // end login
+
+    // update user
+        if(isset($_POST['update_user'])){
+            $idno  = rand(10000, 99999);
+            $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+            $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $pin = $_POST['pin'];
         
-        }else{
-           $error = '
-           <div class="pt-3"></div>
-           <div class="login_error">
-           <strong>Error:</strong> 
-           The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
-           </div>
-           ';
-        }
-
-    };
-// end login
-
-// update user
-    if(isset($_POST['update_user'])){
-        $idno  = rand(10000, 99999);
-        $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-        $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $pin = $_POST['pin'];
-    
-    
-        date_default_timezone_set('America/Denver');
-        $date = date('F d, Y, g:i a', time());
-    
-        $insert = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', username = '$username', account_link = '$account_link', email = '$email', pin = '$pin' WHERE user_id = '".$_POST['user_id']."'";
-        mysqli_query($conn, $insert);
-        //header("location: /");
-        $success = '
-           <div class="pt-3"></div>
-           <div class="login_success">
-           <strong>Success:</strong> 
-           Updated successfully!
-           </div>
-           ';
-    
-      };
-// end update income
-?>
+        
+            date_default_timezone_set('America/Denver');
+            $date = date('F d, Y, g:i a', time());
+        
+            $insert = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', username = '$username', account_link = '$account_link', email = '$email', pin = '$pin' WHERE user_id = '".$_POST['user_id']."'";
+            mysqli_query($conn, $insert);
+            //header("location: /");
+            $success = '
+               <div class="pt-3"></div>
+               <div class="success">
+               <strong>Success:</strong> 
+               Updated successfully!
+               </div>
+               ';
+        
+          };
+    // end update income
+    ?>
+<!-- end php functions -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -105,11 +107,8 @@ date_default_timezone_set('America/Denver');
 
     <!-- custom styles -->
         <link rel="stylesheet" href="../../style.css">
-    <!-- end custom styles -->
-
-    <style>
-
-            .login_success {
+        <style>
+            .success {
                 width: 95%;
                 border-left: 4px solid #00a32a;
                 padding: 12px;
@@ -121,20 +120,15 @@ date_default_timezone_set('America/Denver');
                 box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
                 word-wrap:break-word
             }
-            /* .login .success {
-                border-left-color:#00a32a
-            } 
-            .login_success {
-                border-left-color:#36d639;
-            } */
-</style>
+        </style>
+    <!-- end custom styles -->
 
 </head>
 <body style="background-color: rgb(78, 78, 78);">
 
 
+<!-- php code -->
     <?php 
-
     $user_id = $_GET['id'];
     $select = " SELECT * FROM users WHERE user_id = '$user_id'";
     $result = mysqli_query($conn, $select);
@@ -149,15 +143,13 @@ date_default_timezone_set('America/Denver');
             $account_link   = $row['account_link'];
             $pin        = $row['pin'];
     }}
-
     ?>
 
-    <!-- end php code -->
+<!-- end php code -->
 
 
     <div class="container">
 
-        <!-- <section class="" data-page="income"> -->
             <div class="mt-4"></div>
             <h2 class="text-white">
                 View User
@@ -208,9 +200,10 @@ date_default_timezone_set('America/Denver');
 
     </div>
 
-    <!-- custom script -->
+<!-- custom script -->
     <script src="script.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<!-- end custome script -->
 </body>
 </html>
