e10a2e7686c577a397f225dbbdaa94d04c49bcd9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 15:22:36 -0600

    udpate

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ session_start();
     <!-- end bootstrap -->
 
     <!-- custom styles -->
-        <link rel="stylesheet" href="style.css?v=1.12">
+        <link rel="stylesheet" href="style.css?v=1.13">
         <style>
             .success {
                 width: 70%;
