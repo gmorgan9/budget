3aa1cf2203852e1edfc64ed717fd7679e1cd3616
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 15:07:23 -0600

    update

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ session_start();
     <!-- end bootstrap -->
 
     <!-- custom styles -->
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css?v=1.01">
         <style>
             .success {
                 width: 70%;
