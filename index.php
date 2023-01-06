22f6e7a2f9995f2029be1d35c84ce4300a5462dd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 14:57:56 -0600

    update

--- index.php
+++ index.php
@@ -175,7 +175,7 @@ session_start();
                 margin-left: auto;
                 margin-right: auto;
                 border-radius: 15px;
-                margin-top: 10%;
+                margin-top: 25%;
             }
         </style>
     <!-- end custom styles -->
