b8a5a859b2dc8a5609f958b26735d07e00fb9d95
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 16:23:00 -0600

    update

--- index.php
+++ index.php
@@ -669,11 +669,12 @@ session_start();
                 <h4 class="text-center text-white">
                     <?php echo $firstname; ?> <?php echo $lastname; ?>
                 </h4>
-                <p>
-                    <?php echo $user_idno; ?>
+                <p class="text-center" class="color: rgb(242, 247, 253) !important;">
+                    <?php echo $user_idno; ?> <br>
+                    <?php echo $account_link; ?>
                 </p>
                 <p>
-                    <?php echo $account_link; ?>
+                    
                 </p>
             </div>
             <div class="mt-5"></div>
