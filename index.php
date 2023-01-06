b1ccb3a08d47bd21a0eb18be47414d929d9237df
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 08:49:17 -0600

    update

--- index.php
+++ index.php
@@ -162,7 +162,7 @@ session_start();
 
             <form class="form" action="" method="POST">
             <input type="hidden" id="user_login" name="account_link" value="94768" class="form-control">
-            <div style="background-color: rgb(100, 100, 100); padding: 15px; width: 70%; margin-left: auto; margin-right: auto; border-radius: 15px;" class="username">
+            <div style="background-color: rgb(100, 100, 100); padding: 15px; padding-bottom: -10px; width: 70%; margin-left: auto; margin-right: auto; border-radius: 15px;" class="username">
                 <label class="text-white fw-bold fs-3" for="user_login">Account ID</label>
                 <p class="fs-6" style="color: rgb(25, 25, 25);">
                 94768
