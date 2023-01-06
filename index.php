6af959f150d6d4093cf0303111d92acc7a6e9a74
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-06 10:40:55 -0600

    update

--- index.php
+++ index.php
@@ -3,137 +3,139 @@ require_once "database/connection.php";
 require_once "path.php";
 session_start();
 ?>
-<?php
-// login
-    if(isset($_POST['login'])){
-        // $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
-        $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
-        $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-        $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-        $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $pin = $_POST['pin'];
-        // $cpassword = $_POST['cpassword'];
-        $isadmin = $_POST['isadmin'];
-        $loggedin = $_POST['loggedin'];
-        $account_link = $_POST['account_link'];
-        
-        $select = " SELECT * FROM users WHERE account_link = '$account_link' && pin = '$pin' ";
-        
-        $result = mysqli_query($conn, $select);
-        
-        if(mysqli_num_rows($result) > 0){
-        
-           $row = mysqli_fetch_array($result);
-           $sql = "UPDATE users SET loggedin='1' WHERE pin='$pin'";
-           if (mysqli_query($conn, $sql)) {
-              echo "Record updated successfully";
-            } else {
-              echo "Error updating record: " . mysqli_error($conn);
+<!-- php functions -->
+    <?php
+    // login
+        if(isset($_POST['login'])){
+            // $idno  = rand(1000000, 9999999); // figure how to not allow duplicates
+            $user_id = mysqli_real_escape_string($conn, $_POST['user_id']);
+            $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+            $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $pin = $_POST['pin'];
+            // $cpassword = $_POST['cpassword'];
+            $isadmin = $_POST['isadmin'];
+            $loggedin = $_POST['loggedin'];
+            $account_link = $_POST['account_link'];
+
+            $select = " SELECT * FROM users WHERE account_link = '$account_link' && pin = '$pin' ";
+
+            $result = mysqli_query($conn, $select);
+
+            if(mysqli_num_rows($result) > 0){
+            
+               $row = mysqli_fetch_array($result);
+               $sql = "UPDATE users SET loggedin='1' WHERE pin='$pin'";
+               if (mysqli_query($conn, $sql)) {
+                  echo "Record updated successfully";
+                } else {
+                  echo "Error updating record: " . mysqli_error($conn);
+                }
+                $_SESSION['firstname']         = $row['firstname'];
+                $_SESSION['user_id']          = $row['user_id'];
+                $_SESSION['loggedin']         = $row['loggedin'];
+                $_SESSION['user_idno']        = $row['idno'];
+                $_SESSION['lastname']         = $row['lastname'];
+                $_SESSION['username']         = $row['username'];
+                $_SESSION['email']            = $row['email'];
+                $_SESSION['pin']             = $row['pin'];
+                // $_SESSION['cpass']            = $row['cpassword'];
+                // header('location:' . BASE_URL . '/');
+                header('location: /');
+            
+            }else{
+               $error = '
+               <div class="pt-3"></div>
+               <div class="login_error">
+               <strong>Error:</strong> 
+               The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
+               </div>
+               ';
             }
-            $_SESSION['firstname']         = $row['firstname'];
-            $_SESSION['user_id']          = $row['user_id'];
-            $_SESSION['loggedin']         = $row['loggedin'];
-            $_SESSION['user_idno']        = $row['idno'];
-            $_SESSION['lastname']         = $row['lastname'];
-            $_SESSION['username']         = $row['username'];
-            $_SESSION['email']            = $row['email'];
-            $_SESSION['pin']             = $row['pin'];
-            // $_SESSION['cpass']            = $row['cpassword'];
-            // header('location:' . BASE_URL . '/');
-            header('location: /');
+
+        };
+    // end login
+
+    // add expense
+        if(isset($_POST['exp'])){
+            $idno  = rand(10000, 99999); // figure how to not allow duplicates
+            $description = mysqli_real_escape_string($conn, $_POST['description']);
+            $amount = mysqli_real_escape_string($conn, $_POST['amount']);
+            $comments = mysqli_real_escape_string($conn, $_POST['comments']);
+            $date_spent = mysqli_real_escape_string($conn, $_POST['date_spent']);
+            $cat_idno = mysqli_real_escape_string($conn, $_POST['cat_idno']);
+            $card_idno = mysqli_real_escape_string($conn, $_POST['card_idno']);
+            $person_fn = mysqli_real_escape_string($conn, $_POST['person_fn']);
+            $person_ln = mysqli_real_escape_string($conn, $_POST['person_ln']);
+            $person_idno = mysqli_real_escape_string($conn, $_POST['person_idno']);
+            $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
+            // $created_date = date("F j, Y");
+            // $created_time = date("g:i a");
         
-        }else{
-           $error = '
-           <div class="pt-3"></div>
-           <div class="login_error">
-           <strong>Error:</strong> 
-           The username <strong>'. $_POST['username'] .'</strong> or password entered is not registered on this site. Please try again.
-           </div>
-           ';
+            $select = " SELECT * FROM expenses WHERE idno = '$idno'";
+        
+            $result = mysqli_query($conn, $select);
+        
+            if(mysqli_num_rows($result) > 0){
+            
+                $error[] = 'expense already exist!';
+            
+            }else {
+                $insert = "INSERT INTO expenses (idno, description, amount, comments, person_idno, cat_idno, person_fn, person_ln, account_link, card_idno, date_spent) VALUES ('$idno', '$description','$amount','$comments','$person_idno','$cat_idno', '$person_fn', '$person_ln', '$account_link', '$card_idno', '$date_spent')";
+                mysqli_query($conn, $insert);
+                header('location: /');
+            }
+        
+        };
+    // end add expense
+
+    // add income
+        if(isset($_POST['inc'])){
+            $idno  = rand(10000, 99999); // figure how to not allow duplicates
+            $description = mysqli_real_escape_string($conn, $_POST['description']);
+            $amount = mysqli_real_escape_string($conn, $_POST['amount']);
+            $comments = mysqli_real_escape_string($conn, $_POST['comments']);
+            $date_gained = mysqli_real_escape_string($conn, $_POST['date_gained']);
+            $cat_idno = mysqli_real_escape_string($conn, $_POST['cat_idno']);
+            $card_idno = mysqli_real_escape_string($conn, $_POST['card_idno']);
+            $person_fn = mysqli_real_escape_string($conn, $_POST['person_fn']);
+            $person_ln = mysqli_real_escape_string($conn, $_POST['person_ln']);
+            $person_idno = mysqli_real_escape_string($conn, $_POST['person_idno']);
+            $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
+            // $created_date = date("F j, Y");
+            // $created_time = date("g:i a");
+
+            $select = " SELECT * FROM income WHERE idno = '$idno'";
+
+            $result = mysqli_query($conn, $select);
+
+            if(mysqli_num_rows($result) > 0){
+
+                $error[] = 'income already exist!';
+
+            }else {
+                $insert = "INSERT INTO income (idno, description, amount, comments, person_idno, cat_idno, person_fn, person_ln, account_link, card_idno, date_gained) VALUES ('$idno', '$description','$amount','$comments','$person_idno','$cat_idno', '$person_fn', '$person_ln', '$account_link', '$card_idno', '$date_gained')";
+                mysqli_query($conn, $insert);
+                header('location: /');
+            }
+
+        };
+    // end add income
+
+    // success message
+        if(isset($_GET['success'])) {
+            $success = '
+               <div class="pt-3"></div>
+               <div class="success">
+               <strong>Success:</strong> 
+               Your request has been sent!
+               </div>
+               ';
         }
-
-    };
-// end login
-
-// add expense
-    if(isset($_POST['exp'])){
-        $idno  = rand(10000, 99999); // figure how to not allow duplicates
-        $description = mysqli_real_escape_string($conn, $_POST['description']);
-        $amount = mysqli_real_escape_string($conn, $_POST['amount']);
-        $comments = mysqli_real_escape_string($conn, $_POST['comments']);
-        $date_spent = mysqli_real_escape_string($conn, $_POST['date_spent']);
-        $cat_idno = mysqli_real_escape_string($conn, $_POST['cat_idno']);
-        $card_idno = mysqli_real_escape_string($conn, $_POST['card_idno']);
-        $person_fn = mysqli_real_escape_string($conn, $_POST['person_fn']);
-        $person_ln = mysqli_real_escape_string($conn, $_POST['person_ln']);
-        $person_idno = mysqli_real_escape_string($conn, $_POST['person_idno']);
-        $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
-        // $created_date = date("F j, Y");
-        // $created_time = date("g:i a");
-  
-        $select = " SELECT * FROM expenses WHERE idno = '$idno'";
-  
-        $result = mysqli_query($conn, $select);
-  
-        if(mysqli_num_rows($result) > 0){
-  
-            $error[] = 'expense already exist!';
-  
-        }else {
-            $insert = "INSERT INTO expenses (idno, description, amount, comments, person_idno, cat_idno, person_fn, person_ln, account_link, card_idno, date_spent) VALUES ('$idno', '$description','$amount','$comments','$person_idno','$cat_idno', '$person_fn', '$person_ln', '$account_link', '$card_idno', '$date_spent')";
-            mysqli_query($conn, $insert);
-            header('location: /');
-        }
-  
-    };
-// end add expense
-
-// add income
-    if(isset($_POST['inc'])){
-        $idno  = rand(10000, 99999); // figure how to not allow duplicates
-        $description = mysqli_real_escape_string($conn, $_POST['description']);
-        $amount = mysqli_real_escape_string($conn, $_POST['amount']);
-        $comments = mysqli_real_escape_string($conn, $_POST['comments']);
-        $date_gained = mysqli_real_escape_string($conn, $_POST['date_gained']);
-        $cat_idno = mysqli_real_escape_string($conn, $_POST['cat_idno']);
-        $card_idno = mysqli_real_escape_string($conn, $_POST['card_idno']);
-        $person_fn = mysqli_real_escape_string($conn, $_POST['person_fn']);
-        $person_ln = mysqli_real_escape_string($conn, $_POST['person_ln']);
-        $person_idno = mysqli_real_escape_string($conn, $_POST['person_idno']);
-        $account_link = mysqli_real_escape_string($conn, $_POST['account_link']);
-        // $created_date = date("F j, Y");
-        // $created_time = date("g:i a");
-
-        $select = " SELECT * FROM income WHERE idno = '$idno'";
-
-        $result = mysqli_query($conn, $select);
-
-        if(mysqli_num_rows($result) > 0){
-
-            $error[] = 'income already exist!';
-
-        }else {
-            $insert = "INSERT INTO income (idno, description, amount, comments, person_idno, cat_idno, person_fn, person_ln, account_link, card_idno, date_gained) VALUES ('$idno', '$description','$amount','$comments','$person_idno','$cat_idno', '$person_fn', '$person_ln', '$account_link', '$card_idno', '$date_gained')";
-            mysqli_query($conn, $insert);
-            header('location: /');
-        }
-
-    };
-// end add income
-
-// success message
-    if(isset($_GET['success'])) {
-        $success = '
-           <div class="pt-3"></div>
-           <div class="success">
-           <strong>Success:</strong> 
-           Your request has been sent!
-           </div>
-           ';
-    }
-// end success message
-?>
+    // end success message
+    ?>
+<!-- end php functions -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -149,8 +151,9 @@ session_start();
     <title>Budget</title>
 
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <!-- end bootstrap -->
 
     <!-- custom styles -->
         <link rel="stylesheet" href="style.css">
@@ -697,9 +700,10 @@ session_start();
 
 <?php } ?>
 
-    <!-- custom script -->
+<!-- custom script -->
     <script src="script.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<!-- custom scripts -->
 </body>
 </html>
