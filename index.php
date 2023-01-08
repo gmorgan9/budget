5232881be0d33835be512b791ae113df7b2ec4eb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 13:34:03 -0700

    update

--- index.php
+++ index.php
@@ -326,7 +326,7 @@ session_start();
             <div class="mt-3"></div>
             <div class="d-fle flex-row justify-content-center">
 
-                <div class="ms-2 row justify-content-center">
+                <div class="ms-1 row justify-content-center">
                 <div class="card" style="margin-right: 20px; width: 40%; background-color: rgb(255, 255, 255) !important; color: black;">
                         <div class="card-body">
                           <i class="fs-1 bi bi-coin" style="margin-left: -20px; color: rgb(210,210,210)"></i>
