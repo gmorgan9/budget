f7006a73e567a491af68dc4cccad76850c29bca8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 13:32:55 -0700

    update

--- index.php
+++ index.php
@@ -327,8 +327,8 @@ session_start();
             <div class="d-fle flex-row justify-content-center">
 
                 <div class="row justify-content-center">
-
-                <div class="card" style="margin-right: 20px; width: 42%; background-color: rgb(255, 255, 255) !important; color: black;">
+                    <div class="ms-2"></div>
+                <div class="card" style="margin-right: 20px; width: 40%; background-color: rgb(255, 255, 255) !important; color: black;">
                         <div class="card-body">
                           <i class="fs-1 bi bi-coin" style="margin-left: -20px; color: rgb(210,210,210)"></i>
                           <p class="card-title text-start text-muted fw-bold" style="margin-left: -20px; width: 50%; line-height: .95;">Spent So Far</p>
@@ -338,7 +338,7 @@ session_start();
                         </div>
                     </div>
 
-                    <div class="card" style="margin-right: 20px; width: 42%; background-color: rgb(255, 255, 255) !important; color: black;">
+                    <div class="card" style="margin-right: 20px; width: 40%; background-color: rgb(255, 255, 255) !important; color: black;">
                         <div class="card-body">
                           <i class="fs-1 bi bi-coin" style="margin-left: -20px; color: rgb(210,210,210)"></i>
                           <p class="card-title text-start text-muted fw-bold" style="margin-left: -20px; width: 70%; line-height: .95;">Received So Far</p>
