4b64eeb19279f7cf3fa2c3bbecf9297b909bfb2d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 13:29:17 -0700

    update

--- index.php
+++ index.php
@@ -330,7 +330,7 @@ session_start();
 
                 <div class="card" style="margin-right: 20px; width: 40%; background-color: rgb(255, 255, 255) !important; color: black;">
                         <div class="card-body">
-                          <i class="fs-1 bi bi-coin" style="margin-left: -20px; color: rgb(125,125,125)"></i>
+                          <i class="fs-1 bi bi-coin" style="margin-left: -20px; color: rgb(210,210,210)"></i>
                           <p class="card-title text-start text-muted fw-bold" style="margin-left: -20px; width: 50%; line-height: .95;">Spent So Far</p>
                           <p class="card-text fs-5 text-start fw-bold" style="margin-left: -20px;">
                           $14
