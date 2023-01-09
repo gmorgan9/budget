7edf3222990e16a3643e9fb2e9cf8c6cf7c32135
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 23:59:40 -0700

    update

--- index.php
+++ index.php
@@ -366,7 +366,7 @@ session_start();
                         <div class="card-body">
 
                                 <div class="d-flex flex-row">
-                                    <p class="card-title text-muted fw-bold" style="margin-left: -20px;line-height: .95; margin-top: -10px;">Income</p>
+                                    <p class="card-title text-muted fw-bold float-start" style="margin-left: -20px;line-height: .95; margin-top: -10px;">Income</p>
                                     <p class="card-title text-muted float-end" style="line-height: .95; margin-top: -10px;">Received</p>
                                     <hr>
                                 </div>
