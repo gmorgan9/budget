a1188e119857619c9ea700f282526fbbba40d464
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-09 00:47:52 -0700

    update

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ session_start();
     <meta http-equiv="ScreenOrientation" content="autoRotate:disabled">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable = no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable = no, viewport-fit=cover">
     <title>Budget</title>
 
     <!-- bootstrap -->
