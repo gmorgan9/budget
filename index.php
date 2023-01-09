de46d45bc7bb9ad07b926ab8270c28744a595072
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 23:51:53 -0700

    update

--- index.php
+++ index.php
@@ -348,7 +348,7 @@ session_start();
                             </p>
                         </div>
                     </div>
-                    <div class="card mt-5 mb-3" style="width: 40%; background-color: rgb(255, 255, 255) !important; color: black;">
+                    <div class="card mt-5 mb-3" style="border:none; width: 40%; background-color: rgb(255, 255, 255) !important; color: black;">
                         <div class="card-body">
                           <i class="fs-1 bi bi-cash-coin" style="margin-left: -20px; color: rgb(210,210,210)"></i>
                           <p class="card-title text-start text-muted fw-bold" style="margin-left: -20px; width: 70%; line-height: .95;">Received So Far</p>
