9a4953e954dcddf6639efe0ec5062215d196b2d4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-09 01:45:30 -0700

    update

--- index.php
+++ index.php
@@ -393,21 +393,21 @@ session_start();
                                 $query ="SELECT * FROM categories where parent = 'income'";
                                 $result = $conn->query($query);
                                 if($result->num_rows> 0){
-                                  $options= mysqli_fetch_all($result, MYSQLI_ASSOC);
+                                  $income= mysqli_fetch_all($result, MYSQLI_ASSOC);
                                 }
-                                foreach ($options as $option) {
+                                foreach ($income as $i) {
 
-                                    $cat_idno = $option['idno'];
+                                    $cat_idno = $i['idno'];
 
                                     $sql="SELECT count('1') FROM income WHERE date_gained BETWEEN '$first_day' AND '$last_day' AND cat_idno = '$cat_idno' AND account_link = '$account_link'";
                                     $result=mysqli_query($conn,$sql);
                                     $rowtotal=mysqli_fetch_array($result); 
-                                    $count_m_expenses = $rowtotal[0];
+                                    $count_income = $rowtotal[0];
 
                                     $sql="SELECT sum(amount) FROM income WHERE date_gained BETWEEN '$first_day' AND '$last_day' AND cat_idno = '$cat_idno' AND account_link = '$account_link'";
                                     $result=mysqli_query($conn,$sql);
-                                    $month_expenses=mysqli_fetch_array($result); 
-                                    $m_expenses = $month_expenses[0];
+                                    $month_income=mysqli_fetch_array($result); 
+                                    $m_income = $month_income[0];
                                     
                             ?>
                         <!-- end php code -->
@@ -415,15 +415,15 @@ session_start();
                           <p class="card-text fs-5 text-start fw-bold">
                             <div class="row" style="margin-top: -25px !important;">
                               <div class="col-8 text-start" style="margin-left: -10px;">
-                                  <?php echo $option['category']; ?>
+                                  <?php echo $i['category']; ?>
                               </div>
                               <div class="col text-end pb-1" style="">
                                 <!-- php code -->
                                     <?php
-                                        if($count_m_expenses == 0){
+                                        if($count_income == 0){
                                             echo "$0.00";
                                         } else {
-                                             echo "$$m_expenses";
+                                             echo "$$m_income";
                                         }
                                     ?>
                                 <!-- end php code -->
@@ -470,12 +470,12 @@ session_start();
                                     $sql="SELECT count('1') FROM income WHERE date_gained BETWEEN '$first_day' AND '$last_day' AND cat_idno = '$cat_idno' AND account_link = '$account_link'";
                                     $result=mysqli_query($conn,$sql);
                                     $rowtotal=mysqli_fetch_array($result); 
-                                    $count_m_expenses = $rowtotal[0];
+                                    $count_giving = $rowtotal[0];
                                 
                                     $sql="SELECT sum(amount) FROM income WHERE date_gained BETWEEN '$first_day' AND '$last_day' AND cat_idno = '$cat_idno' AND account_link = '$account_link'";
                                     $result=mysqli_query($conn,$sql);
-                                    $month_expenses=mysqli_fetch_array($result); 
-                                    $m_expenses = $month_expenses[0];
+                                    $month_giving=mysqli_fetch_array($result); 
+                                    $m_giving = $month_giving[0];
 
                             ?>
                         <!-- end php code -->
@@ -489,10 +489,10 @@ session_start();
                               <div class="col text-end pb-1" style="">
                                 <!-- php code -->
                                     <?php
-                                        if($count_m_expenses == 0){
+                                        if($count_giving == 0){
                                             echo "$0.00";
                                         } else {
-                                             echo "$$m_expenses";
+                                             echo "$$m_giving";
                                         }
                                     ?>
                                 <!-- end php code -->
@@ -516,9 +516,59 @@ session_start();
                                 <p class="card-title text-muted fw-bold float-start" style="margin-left: -10px;">Housing</p>
                                 <p class="card-title text-muted float-end">Spent</p>
                             </div>
-                            <hr style="width: 105%; margin-left: -10px;">
-                          <p class="card-text fs-5 text-start fw-bold">
-                          $14
+
+                        <!-- php code -->
+                            <?php
+
+                                $month_year = date('F Y');
+                                $firstday = strtotime("first day of ". $month_year);
+                                $first_day = date('Y-m-d', $firstday);
+                                $month_year = date('F Y');
+                                $lastday = strtotime("last day of ". $month_year);
+                                $last_day = date('Y-m-d', $lastday);
+
+                                $query ="SELECT * FROM categories where parent = 'housing'";
+                                $result = $conn->query($query);
+                                if($result->num_rows> 0){
+                                  $housing= mysqli_fetch_all($result, MYSQLI_ASSOC);
+                                }
+                                foreach ($housing as $h) {
+                                
+                                    $cat_idno = $h['idno'];
+                                
+                                    $sql="SELECT count('1') FROM expenses WHERE date_spent BETWEEN '$first_day' AND '$last_day' AND cat_idno = '$cat_idno' AND account_link = '$account_link'";
+                                    $result=mysqli_query($conn,$sql);
+                                    $rowtotal=mysqli_fetch_array($result); 
+                                    $count_housing = $rowtotal[0];
+                                
+                                    $sql="SELECT sum(amount) FROM expenses WHERE date_spent BETWEEN '$first_day' AND '$last_day' AND cat_idno = '$cat_idno' AND account_link = '$account_link'";
+                                    $result=mysqli_query($conn,$sql);
+                                    $month_housing=mysqli_fetch_array($result); 
+                                    $m_housing = $month_housing[0];
+
+                            ?>
+                        <!-- end php code -->
+
+                            
+                        <p class="card-text fs-5 text-start fw-bold">
+                            <div class="row" style="margin-top: -25px !important;">
+                              <div class="col-8 text-start" style="margin-left: -10px;">
+                                  <?php echo $h['category']; ?>
+                              </div>
+                              <div class="col text-end pb-1" style="">
+                                <!-- php code -->
+                                    <?php
+                                        if($count_housing == 0){
+                                            echo "$0.00";
+                                        } else {
+                                             echo "$$m_housing";
+                                        }
+                                    ?>
+                                <!-- end php code -->
+                              </div>
+                              <hr>
+                            </div>
+                            <?php } ?>
                           </p>
                         </div>
                     </div>
