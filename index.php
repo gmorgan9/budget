8b4f8700ce6876c3d22bd0bc44ba4d3b35bde44a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 23:44:07 -0700

    update

--- index.php
+++ index.php
@@ -160,10 +160,9 @@ session_start();
         <link rel="stylesheet" href="style.css?v=1.25">
         <style>
             .home_page {
-                margin-left: -11px !important; 
-                margin-right: -11px !important;
+                margin-left: -12px !important; 
+                margin-right: -12px !important;
                 padding: 0 !important;
-                margin: 0 !important;
                 background-color: #c0c0c0;
                 box-sizing: border-box;
             }
