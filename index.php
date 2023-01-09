bc24396328610f05110cc7f6a863d261bab4009c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 22:51:29 -0700

    update

--- index.php
+++ index.php
@@ -329,7 +329,7 @@ session_start();
         
             <div class="mt-3"></div>
 
-            <section class="main" style="background-color: rgb(215,215,215);">
+            <div class="main" style="background-color: rgb(215,215,215);">
 
             <!-- top -->
                 <div class="row">
@@ -494,7 +494,7 @@ session_start();
             <!-- end total -->
 
 
-                                    </section>
+            </div>
 
             
 
