6e64cfd8d8661749acc20a3a64f010d8ae4059cd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-08 23:00:19 -0700

    update

--- index.php
+++ index.php
@@ -329,7 +329,7 @@ session_start();
         
             <div class="mt-3"></div>
 
-            <div style="background-color: rgb(215,215,215); width: 110%;">
+            <div style="background-color: rgb(215,215,215); width: 100%;">
 
             <!-- top -->
                 <div class="row">
