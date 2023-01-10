f4a968d7b3606879bc226a01802ad7787235eab6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 16:19:03 -0700

    update

--- index.php
+++ index.php
@@ -1189,7 +1189,12 @@ session_start();
                     <div class="card" style="border: none; background-color: rgb(255, 255, 255) !important; color: black;">
                     
                         <div class="card-body">
-                            <button class="navbar-link" data-nav-link>Deleted</button>
+
+                        <div id="myDIV">
+                            <button class="link">Deleted</button>
+                        </div>
+
+                            
                             <a style="text-decoration: none; color: black;" href="/entry/">
                                 <h5 class="card-title">
                                     <i class="bi bi-trash me-3" style="color: #327ba8; margin-left: -10px;"></i>  Deleted <i class="bi bi-chevron-right float-end" style="color: #327ba8;"></i>
