35b0a9682066086f9bbd76fc960873d1b3f28028
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 11:21:51 -0700

    update

--- index.php
+++ index.php
@@ -1178,68 +1178,94 @@ session_start();
 
             <div class="mt-4"></div>
 
-            <form action="" class="" method="POST">
-                <input type="hidden" class="form-control" name="person_fn" value="<?php echo $firstname;?>">
-                <input type="hidden" class="form-control" name="person_ln" value="<?php echo $lastname;?>">
-                <input type="hidden" class="form-control" name="person_idno" value="<?php echo $user_idno;?>">
-                <input type="hidden" class="form-control" name="account_link" value="<?php echo $account_link;?>">
-                <div class="mb-3">
-                    <label for="desc" class="form-label text-white">Description &nbsp;<span style="font-size: 10px; color: rgb(169, 169, 169);">e.g "McDonalds"</span></label>
-                    <input type="text" name="description" class="form-control" id="desc">
-                </div>
-                <label for="amount" class="form-label text-white">Amount &nbsp;<span style="font-size: 10px; color: rgb(169, 169, 169);">e.g 130.40</span></label>
-                <div class="mb-3 input-group">
-                    <span class="input-group-text" id="basic-addon1">$</span>
-                    <input type="text" name="amount" class="form-control" id="amount">
-                </div>
-                <div class="mb-3">
-                    <label for="date" class="form-label text-white">Date Spent</label>
-                    <input type="date" name="date_spent" class="form-control" id="amount">
-                </div>
-                <div class="row">
-                <div class="mb-3 w-50">
-                    <label for="category" class="form-label text-white">Category</label>
-                    <select name="cat_idno" id="category" class="form-control">
-                        <option value="">Select one...</option>
-                        <?php
-                        $query ="SELECT * FROM categories";
-                        $result = $conn->query($query);
-                        if($result->num_rows> 0){
-                          $options= mysqli_fetch_all($result, MYSQLI_ASSOC);
-                        }
-                        ?>
-                        <?php 
-                            foreach ($options as $option) {
-                        ?>
-                            <option value="<?php echo $option['idno']; ?>"><?php echo $option['category']; ?></option>
-                        <?php } ?>
-                    </select>
-                </div>
-                <div class="mb-3 w-50">
-                    <label for="date" class="form-label text-white">Card</label>
-                    <select name="card_idno" id="card" class="form-control">
-                        <option value="">Select one...</option>
-                        <?php
-                        $query ="SELECT * FROM cards";
-                        $result = $conn->query($query);
-                        if($result->num_rows> 0){
-                          $options= mysqli_fetch_all($result, MYSQLI_ASSOC);
-                        }
-                        ?>
-                        <?php 
-                            foreach ($options as $option) {
-                        ?>
-                            <option value="<?php echo $option['idno']; ?>"><?php echo $option['name']; ?></option>
-                        <?php } ?>
-                    </select>
-                </div>
-                </div>
-                <div class="mb-3">
-                    <label for="comment" class="form-label text-white">Comments</label>
-                    <textarea class="form-control" name="comments" id="comment"></textarea>
-                </div>
-                <button type="submit" name="exp" class="mt-3 btn" style="background-color: rgb(242, 247, 253);">Submit</button>
-            </form>
+
+            <div class="mt-4"></div>
+            <?php 
+                $month_year = date('F Y');
+                
+            ?>
+            <h2 class="text-white">
+                <?php echo $month_year; ?> Expenses
+            </h2>
+            
+            <p class="" style="color: rgb(242, 247, 253);">
+                Compliled list of Expenses for <?php echo $month_year; ?>.
+            </p>
+            <hr style="color: rgb(242, 247, 253);">
+            <div class="mt-4"></div>
+
+            <?php 
+                $month_year = date('F Y');
+                $firstday = strtotime("first day of ". $month_year);
+                $first_day = date('Y-m-d', $firstday);
+                $month_year = date('F Y');
+                $lastday = strtotime("last day of ". $month_year);
+                $last_day = date('Y-m-d', $lastday);
+                
+                ?>
+                <p class="text-white">
+                    <?php 
+
+                    $sql="SELECT count('1') FROM expenses WHERE date_spent BETWEEN '$first_day' AND '$last_day' AND account_link = '$account_link'";
+                    $result=mysqli_query($conn,$sql);
+                    $rowtotal=mysqli_fetch_array($result); 
+                    echo "<b>Expense Records:</b> $rowtotal[0]";
+
+                    ?>
+                </p>
+
+
+            <table class=" table table-bordered">
+                <thead style="background-color: white;">
+                  <tr>
+                    <th class="text-center" scope="col-1">ID #</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Amount</th>
+                    <th class="text-center" scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody class="table-group-divider" style="background-color: #f0f0f0;">
+
+                <?php
+
+                    $sql = "SELECT * FROM expenses WHERE date_spent BETWEEN '$first_day' AND '$last_day' AND account_link = '$account_link' ORDER BY date_spent DESC ";
+                    $all = mysqli_query($conn, $sql);
+                    if($all) {
+                        while ($row = mysqli_fetch_assoc($all)) {
+                          $exp_id         = $row['exp_id'];
+                          $idno           = $row['idno'];
+                          $description    = $row['description'];
+                          $amount         = $row['amount'];
+                          ?>
+                  <tr>
+                      <th class="text-center" scope="row" style="width: 15px;"><?php echo $idno; ?></th>
+                      <td><?php echo $description; ?></td>
+                      <td>$<?php echo $amount; ?></td>
+                      <td style="width: 20px;">
+                        <div class="d-flex justify-content-center">
+                          <a style="text-decoration: none; background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" href="view/index.php?id=<?php echo $exp_id; ?>"><span class="badge text-bg-success">View</span></a>
+                          &nbsp;
+                          <form method="post" action="">
+                            <input type="hidden" name="exp_id" value="<?php echo $exp_id; ?>" />
+                            <button onclick="return confirm('Be Careful, Can\'t be undone! \r\nOK to delete?')" style="background: none; color: inherit; border: none; padding: 0; font: inherit; cursor: pointer; outline: inherit;" type="submit" name="delete"><span class="badge text-bg-danger">Delete</span></button>
+                          </form>
+                        </div>
+                      </td>
+                      <?php }}?>
+                </tbody>
+                <thead class="table-group-divider" style="background-color: white;">
+                  <tr>
+                    <th class="text-center" scope="col">ID #</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Amount</th>
+                    <th class="text-center" scope="col">Actions</th>
+                  </tr>
+                </thead>
+
+                
+            </table>
+            
+
         </section>
 
         <section class="" data-page="income">
