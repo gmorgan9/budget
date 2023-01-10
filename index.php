d29093106601968dafaa5da1d31f1f0592cb3458
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 13:45:15 -0700

    update

--- index.php
+++ index.php
@@ -1176,9 +1176,10 @@ session_start();
                 <div class="row d-flex justify-content-center" style="margin-top: 165px; background-color: rgb(245, 245, 245);">
                 
                     <div class="card" style="border: none; background-color: rgb(255, 255, 255) !important; color: black;">
-                    <a class="stretched-link" style="text-decoration: none; color: black;" href="/entry/">
+                    
                         <div class="card-body" style="margin-bottom: -10px;">
                             <div>
+                            <a class="stretched-link" style="text-decoration: none; color: black;" href="/entry/">text</a>
                                 <h5 class="card-title">
                                     <i class="bi bi-trash me-3" style="color: #327ba8; margin-left: -10px;"></i>  Deleted <i class="bi bi-chevron-right float-end" style="color: #327ba8;"></i>
                                     
@@ -1186,7 +1187,7 @@ session_start();
                                 
                             </div>
                         </div>
-                        </a>
+                        <!-- </a> -->
                     </div>
                 </div>
                 
