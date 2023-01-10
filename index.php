2dee126d08a6411b5709b51d2840ccf3b9b20972
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 13:48:05 -0700

    update

--- index.php
+++ index.php
@@ -187,7 +187,7 @@ session_start();
                 margin-left: -12px !important;
                 z-index: 1;
                 background-color: #3e4881 !important;
-                position: fixed; 
+                position: sticky; 
                 top: 0 !important; 
                 width: 100%;
                 height: 120px !important;
