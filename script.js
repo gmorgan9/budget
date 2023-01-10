706436f0d0097f8053e54af46a97bbbd7210e1b0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 14:53:33 -0700

    update

--- script.js
+++ script.js
@@ -23,28 +23,28 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-// page navigation variables
-const newLinks = document.querySelectorAll("[data-new-link]");
-//const navigationLink = document.getElementByClass('active');
-const newPages = document.querySelectorAll("[new-page]");
+// // page navigation variables
+// const newLinks = document.querySelectorAll("[data-new-link]");
+// //const navigationLink = document.getElementByClass('active');
+// const newPages = document.querySelectorAll("[new-page]");
 
-// add event to all nav link
-for (let i = 0; i < newLinks.length; i++) {
-  newLinks[i].addEventListener("click", function () {
+// // add event to all nav link
+// for (let i = 0; i < newLinks.length; i++) {
+//   newLinks[i].addEventListener("click", function () {
     
 
-    for (let i = 0; i < newPages.length; i++) {
-      if (this.innerHTML.toLowerCase() === newPages[i].dataset.page) {
-        // console.log(this.innerHTML.toLowerCase());
-        newPages[i].classList.add("active");
-        newLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        newPages[i].classList.remove("active");
-        newLinks[i].classList.remove("active");
-      }
-    }
+//     for (let i = 0; i < newPages.length; i++) {
+//       if (this.innerHTML.toLowerCase() === newPages[i].dataset.page) {
+//         // console.log(this.innerHTML.toLowerCase());
+//         newPages[i].classList.add("active");
+//         newLinks[i].classList.add("active");
+//         window.scrollTo(0, 0);
+//       } else {
+//         newPages[i].classList.remove("active");
+//         newLinks[i].classList.remove("active");
+//       }
+//     }
 
-  });
-}
+//   });
+// }
 
