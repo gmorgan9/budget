ea45fa222847cc94ccd446d0e3b5f61d85df4321
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-09 23:04:50 -0700

    update

--- entry/index.php
+++ entry/index.php
@@ -186,7 +186,7 @@ session_start();
         font-weight: 700; 
         /* border-bottom: 5px solid #3e4881; */
         padding-bottom: unset !important;
-        padding-bottom: 10px !important;
+        padding-bottom: 11px !important;
         transition: background 0.45s ease;
     }
     </style>
