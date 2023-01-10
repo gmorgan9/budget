b8a5528774efc504078edc026fcdf9a094df9417
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 11:43:20 -0700

    update

--- index.php
+++ index.php
@@ -1172,7 +1172,7 @@ session_start();
             <div class="mt-5"></div>
 
             <!-- start -->
-            <div class="trans d-flex flex-column justify-content-center" style="z-index: 1; background-color: pink !important;">
+            <div class="" style="z-index: 1; background-color: pink !important;">
 
             
             <?php 
