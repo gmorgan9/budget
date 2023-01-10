d3528cc1a9e99c6576e375a04c2deaf815cda081
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-09 23:05:36 -0700

    u

--- entry/index.php
+++ entry/index.php
@@ -187,6 +187,8 @@ session_start();
         /* border-bottom: 5px solid #3e4881; */
         padding-bottom: unset !important;
         padding-top: 5px !important;
+        margin-top: auto;
+        margin-bottom: auto;
         padding-bottom: 11px !important;
         transition: background 0.45s ease;
     }
