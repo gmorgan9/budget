acd7d66fd8061e3d2fc05586158dc223777e3377
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 13:18:29 -0700

    update

--- index.php
+++ index.php
@@ -1176,8 +1176,7 @@ session_start();
                     <div class="card" style="border: none; background-color: rgb(255, 255, 255) !important; color: black;">
                         <div class="card-body" style="margin-bottom: -10px;">
                             <div>
-                                <i class="bi bi-trash me-3 text-primary" style="margin-left: -10px;"></i>
-                                <h5 class="card-title">Deleted</h5>
+                                <h5 class="card-title"><i class="bi bi-trash me-3" style="color: #327ba8; margin-left: -10px;"></i>  Deleted</h5>
                             </div>
 
                           <p class="card-text fs-5 text-start fw-bold">
@@ -1189,7 +1188,7 @@ session_start();
 
             <!-- end deleted -->
 
-                <!-- income -->
+            <!-- income -->
                 
                 <div class="row d-flex justify-content-center" style="background-color: rgb(245, 245, 245);">
                     <div class="card mt-3" style="border: none; background-color: rgb(255, 255, 255) !important; color: black;">
