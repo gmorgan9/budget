0121ee572c6d143a7850e66016a2334093d2ff76
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 14:40:25 -0700

    udpate

--- index.php
+++ index.php
@@ -1190,7 +1190,9 @@ session_start();
                     <div class="card" style="border: none; background-color: rgb(255, 255, 255) !important; color: black;">
                     
                         <div class="card-body">
+                        <li class="navbar-item">
                             <button class="navbar-link" data-nav-link>Deleted</button>
+                        </li>
                             <a style="text-decoration: none; color: black;" href="/entry/">
                                 <h5 class="card-title">
                                     <i class="bi bi-trash me-3" style="color: #327ba8; margin-left: -10px;"></i>  Deleted <i class="bi bi-chevron-right float-end" style="color: #327ba8;"></i>
