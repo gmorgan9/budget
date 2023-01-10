ee6e185bbd5937debbc387d71e7e7ab730f6b2b5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 13:52:21 -0700

    update

--- index.php
+++ index.php
@@ -1172,8 +1172,8 @@ session_start();
 
 
             <!--  deleted -->
-                
-                <div class="row d-flex justify-content-center" style="margin-top: 165px; background-color: rgb(245, 245, 245);">
+                <div class="mt-3"></div>
+                <div class="row d-flex justify-content-center" style="background-color: rgb(245, 245, 245);">
                 
                     <div class="card" style="border: none; background-color: rgb(255, 255, 255) !important; color: black;">
                     
