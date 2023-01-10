73c9743fa713914156986c70c50df645664391e0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-09 21:21:19 -0700

    update

--- index.php
+++ index.php
@@ -552,7 +552,7 @@ session_start();
                             </div>
                             <div class="add_form">
                                 <form action="" method="post">
-                                    <input type="text" name="category" style="margin-left: -10px; border:none;" placeholder="Item Label">
+                                    <input type="text" name="category" style="float: left; margin-left: -10px; border:none;" placeholder="Item Label">
                                     <button type="submit" class="btn" name="add_item" style="float:right; font-weight: 500;"><i class="bi bi-plus"></i></button>
                                 </form>
                             </div>
