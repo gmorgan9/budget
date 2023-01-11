979d4385f96375996d3f97a1366ea7021b6b4929
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 18:52:04 -0700

    update

--- index.php
+++ index.php
@@ -1439,6 +1439,20 @@ session_start();
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <!-- custom scripts -->
+<script>
+    function switchVisible() {
+    if (document.getElementById('deleted')) {
 
+        if (document.getElementById('deleted').style.display == 'none') {
+            document.getElementById('deleted').style.display = 'block';
+            document.getElementById('trans').style.display = 'none';
+        }
+        else {
+            document.getElementById('deleted').style.display = 'none';
+            document.getElementById('trans').style.display = 'block';
+        }
+    }
+}
+</script>
 </body>
 </html>
