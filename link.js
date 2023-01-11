0df15a297e569f4eb2715b582b08be61ae696cb4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 18:16:12 -0700

    update

--- link.js
+++ link.js
@@ -11,7 +11,7 @@ for (var i = 0; i < btns.length; i++) {
 
     // If there's no active class
     if (current.length > 0) { 
-      current[0].className = current[0].className.replace(" active", "");
+      current[0].className = current[0].className.replace(" active", " ");
     }
 
     // Add the active class to the current/clicked button
