99020ff647191a42bbbc2426ed1fe969e1542d62
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 18:12:39 -0700

    update

--- index.php
+++ index.php
@@ -1531,6 +1531,7 @@ session_start();
 
 <!-- custom script -->
     <script src="script.js"></script>
+    <script src="link.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <!-- custom scripts -->
