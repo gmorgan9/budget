bc8e48fd01f74c6adba160ffaaaf4903a380f14e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 20:08:33 -0700

    update

--- index.php
+++ index.php
@@ -1283,7 +1283,7 @@ session_start();
 
 
         <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="exampleModal" tabindex="2" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog modal-fullscreen">
                 <div class="modal-content">
                   <div class="modal-header">
@@ -1295,7 +1295,6 @@ session_start();
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                   </div>
                 </div>
               </div>
