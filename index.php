1f5b1d8692a34621512c10ddff6423c2428a605f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 18:53:27 -0700

    udpate

--- index.php
+++ index.php
@@ -1282,6 +1282,7 @@ session_start();
             
 
         <main class="" style="display: none;" id="deleted">
+            <input id="Button1" type="button" value="Click" onclick="switchVisible();"/>
             <div class="mt-4"></div>
             <h2 class="text-white">
                 Deleted
