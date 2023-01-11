8e4b036e6be0924ccebfe28a22a0aac5392d40b9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2023-01-10 18:14:13 -0700

    update

--- link.js
+++ link.js
@@ -1,10 +1,20 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("test");
-var btns = header.getElementsByClassName("link");
+// Get the container element
+var btnContainer = document.getElementById("test");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("link");
+
+// Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
   });
 }
